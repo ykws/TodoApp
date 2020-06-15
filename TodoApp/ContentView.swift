e494435a48d6512b244c33782159b41289cbf93d
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2020-06-15 21:47:59 +0900

    Initial Commit

---
+++ TodoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoApp
+//
+//  Created by KAWASHIMA Yoshiyuki on 2020/06/15.
+//  Copyright © 2020 ykws. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
